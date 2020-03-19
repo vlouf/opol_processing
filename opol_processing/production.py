c8c362917582a88cbe9b63a573d32d319cdba0d7
Valentin Louf <valentin.louf@bom.gov.au>
2020-03-19 14:43:23 +1100

    REM CPOL specific stuffs

--- opol_processing/production.py
+++ opol_processing/production.py
@@ -32,7 +32,6 @@ import numpy as np
 # Custom modules.
 from . import attenuation
 from . import filtering
-from . import hydrometeors
 from . import phase
 from . import radar_codes
 from . import velocity
@@ -56,8 +55,6 @@ def _mkdir(dir):
 
 def process_and_save(radar_file_name,
                      outpath,
-                     sound_dir=None,
-                     instrument='OPOL',
                      do_dealiasing=True,
                      use_unravel=True):
     """
@@ -71,8 +68,6 @@ def process_and_save(radar_file_name,
         Path for saving output data.
     sound_dir: str
         Path to radiosoundings directory.
-    instrument: str
-        Name of radar (only OPOL will change something).
     do_dealiasing: bool
         Dealias velocity.
     use_unravel: bool
@@ -110,11 +105,7 @@ def process_and_save(radar_file_name,
     _mkdir(outpath_ppi)
 
     # Generate output file name.
-    if instrument == 'OPOL':
-        outfilename = "twp10cpolppi.b1.{}00.nc".format(radar_start_date.strftime("%Y%m%d.%H%M"))
-    else:
-        outfilename = "cfrad." + radar_start_date.strftime("%Y%m%d_%H%M%S") + ".nc"
-
+    outfilename = "twp10cpolppi.b1.{}00.nc".format(radar_start_date.strftime("%Y%m%d.%H%M"))
     outfilename = os.path.join(outpath_ppi, outfilename)
 
     # Check if output file already exists.
@@ -122,66 +113,62 @@ def process_and_save(radar_file_name,
         print(f"Output file {outfilename} already exists.")
         return None
 
-    if is_cpol:
-        # Lat/lon informations
-        latitude = radar.latitude['data']
-        longitude = radar.longitude['data']
-        maxlon = longitude.max()
-        minlon = longitude.min()
-        maxlat = latitude.max()
-        minlat = latitude.min()
-        origin_altitude = '50'
-        origin_latitude = '-12.2491'
-        origin_longitude = '131.0444'
+    # Lat/lon informations
+    latitude = radar.gate_latitude['data']
+    longitude = radar.gate_longitude['data']
+    maxlon = longitude.max()
+    minlon = longitude.min()
+    maxlat = latitude.max()
+    minlat = latitude.min()
+    origin_altitude = radar.altitude['data'][0]
+    origin_latitude = radar.latitude['data'][0]
+    origin_longitude = radar.longitude['data'][0]
 
-        unique_id = str(uuid.uuid4())
-        metadata = {'Conventions': "CF-1.6, ACDD-1.3",
-                    'acknowledgement': 'This work has been supported by the U.S. Department of Energy Atmospheric Systems Research Program through the grant DE-SC0014063. Data may be freely distributed.',
-                    'country': 'Australia',
-                    'creator_email': 'valentin.louf@bom.gov.au',
-                    'creator_name': 'Valentin Louf',
-                    'creator_url': 'github.com/vlouf',
-                    'date_created': today.isoformat(),
-                    "geospatial_bounds": f"POLYGON(({minlon:0.6} {minlat:0.6},{minlon:0.6} {maxlat:0.6},{maxlon:0.6} {maxlat:0.6},{maxlon:0.6} {minlat:0.6},{minlon:0.6} {minlat:0.6}))",
-                    'geospatial_lat_max': f'{maxlat:0.6}',
-                    'geospatial_lat_min': f'{minlat:0.6}',
-                    'geospatial_lat_units': "degrees_north",
-                    'geospatial_lon_max': f'{maxlon:0.6}',
-                    'geospatial_lon_min': f'{minlon:0.6}',
-                    'geospatial_lon_units': "degrees_east",
-                    'history': "created by Valentin Louf on raijin.nci.org.au at " + today.isoformat() + " using Py-ART",
-                    'id': unique_id,
-                    'institution': 'Bureau of Meteorology',
-                    'instrument': 'radar',
-                    'instrument_name': 'OPOL',
-                    'instrument_type': 'radar',
-                    'keywords': 'radar, tropics, Doppler, dual-polarization',
-                    'licence': "Freely Distributed",
-                    'naming_authority': 'au.org.nci',
-                    'origin_altitude': origin_altitude,
-                    'origin_latitude': origin_latitude,
-                    'origin_longitude': origin_longitude,
-                    'platform_is_mobile': 'false',
-                    'processing_level': 'b1',
-                    'project': "OPOL",
-                    'publisher_name': "NCI",
-                    'publisher_url': "nci.gov.au",
-                    'product_version': f"v{today.year}.{today.month:02}",
-                    'references': 'doi:10.1175/JTECH-D-18-0007.1',
-                    'site_name': 'Gunn Pt',
-                    'source': 'radar',
-                    'state': "NT",
-                    'standard_name_vocabulary': 'CF Standard Name Table v71',
-                    'summary': "Volumetric scan from OPOL dual-polarization Doppler radar (Darwin, Australia)",
-                    'time_coverage_start': radar_start_date.isoformat(),
-                    'time_coverage_end': radar_end_date.isoformat(),
-                    'time_coverage_duration': "P10M",
-                    'time_coverage_resolution': "PT10M",
-                    'title': "radar PPI volume from OPOL",
-                    'uuid': unique_id,
-                    'version': radar.metadata['version']}
-
-        radar.metadata = metadata
+    unique_id = str(uuid.uuid4())
+    metadata = {'Conventions': "CF-1.6, ACDD-1.3",
+                'acknowledgement': 'This work has been supported by the U.S. Department of Energy Atmospheric Systems Research Program through the grant DE-SC0014063. Data may be freely distributed.',
+                'country': 'Australia',
+                'creator_email': 'valentin.louf@bom.gov.au',
+                'creator_name': 'Valentin Louf',
+                'creator_url': 'github.com/vlouf',
+                'date_created': today.isoformat(),
+                "geospatial_bounds": f"POLYGON(({minlon:0.6} {minlat:0.6},{minlon:0.6} {maxlat:0.6},{maxlon:0.6} {maxlat:0.6},{maxlon:0.6} {minlat:0.6},{minlon:0.6} {minlat:0.6}))",
+                'geospatial_lat_max': f'{maxlat:0.6}',
+                'geospatial_lat_min': f'{minlat:0.6}',
+                'geospatial_lat_units': "degrees_north",
+                'geospatial_lon_max': f'{maxlon:0.6}',
+                'geospatial_lon_min': f'{minlon:0.6}',
+                'geospatial_lon_units': "degrees_east",
+                'history': "created by Valentin Louf on gadi.nci.org.au at " + today.isoformat() + " using Py-ART",
+                'id': unique_id,
+                'institution': 'Bureau of Meteorology',
+                'instrument': 'radar',
+                'instrument_name': 'OPOL',
+                'instrument_type': 'radar',
+                'keywords': 'radar, Doppler, dual-polarization, shipborne',
+                'licence': "Freely Distributed",
+                'naming_authority': 'au.org.nci',
+                'origin_altitude': origin_altitude,
+                'origin_latitude': origin_latitude,
+                'origin_longitude': origin_longitude,
+                'platform_is_mobile': 'true',
+                'processing_level': 'b1',
+                'project': "OPOL",
+                'publisher_name': "NCI",
+                'publisher_url': "nci.gov.au",
+                'product_version': f"v{today.year}.{today.month:02}",
+                'site_name': 'RV Investigator',
+                'source': 'radar',
+                'standard_name_vocabulary': 'CF Standard Name Table v71',
+                'summary': "Volumetric scan from OPOL dual-polarization Doppler radar (RV Investigator)",
+                'time_coverage_start': radar_start_date.isoformat(),
+                'time_coverage_end': radar_end_date.isoformat(),
+                'time_coverage_duration': "P06M",
+                'time_coverage_resolution': "PT06M",
+                'title': "radar PPI volume from OPOL",
+                'uuid': unique_id,
+                'version': radar.metadata['version']}
+    radar.metadata = metadata
 
     # Write results
     pyart.io.write_cfradial(outfilename, radar, format='NETCDF4')
@@ -194,10 +181,8 @@ def process_and_save(radar_file_name,
     return None
 
 
-def production_line(radar_file_name, 
-                    sound_dir, 
-                    is_cpol=True, 
-                    do_dealiasing=True, 
+def production_line(radar_file_name,
+                    do_dealiasing=True,
                     use_unravel=True):
     """
     Production line for correcting and estimating OPOL data radar parameters.
@@ -237,9 +222,7 @@ def production_line(radar_file_name,
     10/ Compute Giangrande's PHIDP using pyart.
     11/ Unfold velocity.
     12/ Compute attenuation for ZH
-    13/ Compute attenuation for ZDR
-    14/ Estimate Hydrometeors classification using csu toolbox.
-    15/ Estimate Rainfall rate using csu toolbox.
+    13/ Compute attenuation for ZDR    
     16/ Removing fake/temporary fieds.
     17/ Rename fields to pyart standard names.
     """
@@ -282,11 +265,7 @@ def production_line(radar_file_name,
                         'total_power',
                         'velocity']
 
-    # !!! READING THE RADAR !!!
-    if is_cpol:
-        radar = pyart.io.read(radar_file_name)
-    else:
-        radar = radar_codes.read_radar(radar_file_name)
+    radar = radar_codes.read_radar(radar_file_name)
 
     # Correct data type manually
     try:
@@ -300,95 +279,24 @@ def production_line(radar_file_name,
     if not radar_codes.check_reflectivity(radar):
         raise TypeError(f"Reflectivity field is empty in {radar_file_name}.")
 
-    if not radar_codes.check_azimuth(radar):
-        raise TypeError(f"Azimuth field is empty in {radar_file_name}.")
-
-    if not radar_codes.check_year(radar):
-        print(f'{radar_file_name} date probably wrong. Had to correct century.')
-
-    new_azimuth, azi_has_changed = radar_codes.correct_azimuth(radar)
-    if azi_has_changed:
-        radar.azimuth['data'] = new_azimuth
-
     # Getting radar's date and time.
     radar_start_date = netCDF4.num2date(radar.time['data'][0], radar.time['units'].replace("since", "since "))
     radar.time['units'] = radar.time['units'].replace("since", "since ")
 
-    # Get radiosoundings:
-    if sound_dir is not None:
-        radiosonde_fname = radar_codes.get_radiosoundings(sound_dir, radar_start_date)
-
-    # Correct Doppler velocity units.
-    try:
-        radar.fields['VEL']['units'] = "m s-1"
-        vel_missing = False
-    except KeyError:
-        vel_missing = True
-
-    # Looking for RHOHV field
-    # For CPOL, season 09/10, there are no RHOHV fields before March!!!!
-    try:
-        radar.fields['RHOHV']
-        fake_rhohv = False  # Don't need to delete this field cause it's legit.
-    except KeyError:
-        # Creating a fake RHOHV field.
-        fake_rhohv = True  # We delete this fake field later.
-        rho = pyart.config.get_metadata('cross_correlation_ratio')
-        rho['data'] = np.ones_like(radar.fields['DBZ']['data'])
-        radar.add_field('RHOHV', rho)
-        radar.add_field('RHOHV_CORR', rho)
-
-    # Compute SNR and extract radiosounding temperature.
-    # Requires radiosoundings
-    if sound_dir is not None:
-        try:
-            height, temperature, snr = radar_codes.snr_and_sounding(radar, radiosonde_fname)
-            radar.add_field('temperature', temperature, replace_existing=True)
-            radar.add_field('height', height, replace_existing=True)
-        except ValueError:
-            traceback.print_exc()
-            print(f"Impossible to compute SNR {radar_file_name}")
-            return None
-
-        # Looking for SNR
-        try:
-            radar.fields['SNR']
-        except KeyError:
-            radar.add_field('SNR', snr, replace_existing=True)
-
     # Correct RHOHV
-    if not fake_rhohv:
-        rho_corr = radar_codes.correct_rhohv(radar)
-        radar.add_field_like('RHOHV', 'RHOHV_CORR', rho_corr, replace_existing=True)
+    rho_corr = radar_codes.correct_rhohv(radar)
+    radar.add_field_like('RHOHV', 'RHOHV_CORR', rho_corr, replace_existing=True)
 
     # Correct ZDR
     corr_zdr = radar_codes.correct_zdr(radar)
     radar.add_field_like('ZDR', 'ZDR_CORR', corr_zdr, replace_existing=True)
 
     # GateFilter
-    if is_cpol:
-        gatefilter = filtering.do_gatefilter_cpol(radar,
-                                                  refl_name='DBZ',
-                                                  phidp_name="PHIDP",
-                                                  rhohv_name='RHOHV_CORR',
-                                                  zdr_name="ZDR")
-    else:
-        gatefilter = filtering.do_gatefilter(radar,
-                                             refl_name='DBZ',
-                                             phidp_name="PHIDP",
-                                             rhohv_name='RHOHV_CORR',
-                                             zdr_name="ZDR")
-
-    # Check if NCP exists.
-    try:
-        radar.fields['NCP']
-        fake_ncp = False
-    except KeyError:
-        fake_ncp = True
-        ncp = pyart.config.get_metadata('normalized_coherent_power')
-        ncp['data'] = np.zeros_like(radar.fields['RHOHV']['data'])
-        ncp['data'][gatefilter.gate_included] = 1
-        radar.add_field('NCP', ncp)
+    gatefilter = filtering.do_gatefilter_opol(radar,
+                                                refl_name='DBZ',
+                                                phidp_name="PHIDP",
+                                                rhohv_name='RHOHV_CORR',
+                                                zdr_name="ZDR")
 
     phidp, kdp = phase.phidp_giangrande(radar, gatefilter)
     radar.add_field('PHIDP_VAL', phidp)
@@ -398,12 +306,8 @@ def production_line(radar_file_name,
 
     # Unfold VELOCITY
     if do_dealiasing:
-        if not vel_missing:
-            if is_cpol:
-                vdop_unfold = velocity.unravel(radar, gatefilter, nyquist=13.3)
-            else:
-                vdop_unfold = velocity.unravel(radar, gatefilter)
-            radar.add_field('VEL_UNFOLDED', vdop_unfold, replace_existing=True)
+        vdop_unfold = velocity.unravel(radar, gatefilter)
+        radar.add_field('VEL_UNFOLDED', vdop_unfold, replace_existing=True)
 
     # Correct attenuation ZH and ZDR and hardcode gatefilter
     zh_corr = attenuation.correct_attenuation_zh_pyart(radar, gatefilter, phidp_field=phidp_field_name)
@@ -411,30 +315,6 @@ def production_line(radar_file_name,
 
     zdr_corr = attenuation.correct_attenuation_zdr(radar, gatefilter)
     radar.add_field('ZDR_CORR_ATTEN', zdr_corr)
-
-    # Hydrometeors classification
-    hydro_class = hydrometeors.hydrometeor_classification(radar,
-                                                          gatefilter,
-                                                          kdp_name=kdp_field_name,
-                                                          zdr_name='ZDR_CORR_ATTEN')
-
-    radar.add_field('radar_echo_classification', hydro_class, replace_existing=True)
-
-    # Rainfall rate
-    rainfall = hydrometeors.rainfall_rate(radar,
-                                          gatefilter,
-                                          kdp_name=kdp_field_name,
-                                          refl_name='DBZ_CORR',
-                                          zdr_name='ZDR_CORR_ATTEN')
-    radar.add_field("radar_estimated_rain_rate", rainfall)
-
-    # Removing fake and useless fields.
-    if fake_ncp:
-        radar.fields.pop('NCP')
-
-    if fake_rhohv:
-        radar.fields.pop("RHOHV")
-        radar.fields.pop("RHOHV_CORR")
 
     # Remove obsolete fields:
     for obsolete_key in ["Refl", "PHI_UNF", "PHI_CORR", "height", 'TH', 'TV', 'ZDR_CORR', 'RHOHV']:
@@ -451,10 +331,9 @@ def production_line(radar_file_name,
             continue
 
     # Delete working variables.
-    if is_cpol:
-        for k in list(radar.fields.keys()):
-            if k not in OUTPUT_RADAR_FLD:
-                radar.fields.pop(k)
+    # for k in list(radar.fields.keys()):
+    #     if k not in OUTPUT_RADAR_FLD:
+    #         radar.fields.pop(k)
 
     # Correct the standard_name metadata:
     radar_codes.correct_standard_name(radar)
