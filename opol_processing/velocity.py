649a096dea6553e8ed1d51afed6eca925a6509d1
Valentin Louf <valentin.louf@bom.gov.au>
2020-03-19 16:38:01 +1100

    FIX dealiasing for OPOL

--- opol_processing/velocity.py
+++ opol_processing/velocity.py
@@ -17,28 +17,7 @@ import netCDF4
 import numpy as np
 
 
-def _check_nyquist_velocity(radar, vel_name='VEL'):
-    """
-    Check if Nyquist velocity is present in the instrument parameters. If not,
-    then it is created.
-    """
-    try:
-        vnyq = radar.instrument_parameters['nyquist_velocity']['data']
-        if vnyq is None:
-            raise KeyError('Nyquist velocity does not exists.')
-    except KeyError:
-        vnyq = np.nanmax(radar.fields[vel_name]['data'])
-        nray = len(radar.azimuth['data'])
-        vnyq_array = np.array([vnyq] * nray, dtype=np.float32)
-        nyquist_velocity = pyart.config.get_metadata('nyquist_velocity')
-        nyquist_velocity['data'] = vnyq_array
-        nyquist_velocity['_Least_significant_digit'] = 2
-        radar.instrument_parameters['nyquist_velocity'] = nyquist_velocity
-
-    return vnyq
-
-
-def unravel(radar, gatefilter, vel_name='VEL', dbz_name='DBZ', nyquist=None):
+def unravel(radar, gatefilter, vel_name='VEL', dbz_name='DBZ'):
     """
     Unfold Doppler velocity using Py-ART region based algorithm. Automatically
     searches for a folding-corrected velocity field.
@@ -61,11 +40,7 @@ def unravel(radar, gatefilter, vel_name='VEL', dbz_name='DBZ', nyquist=None):
     """
     import unravel
 
-    vnyq = _check_nyquist_velocity(radar, vel_name)
-    if nyquist is None:
-        if np.isscalar(vnyq):
-            nyquist = vnyq
-
+    nyquist = 13.3
     unfvel = unravel.unravel_3D_pyart(radar,
                                       vel_name,
                                       dbz_name,
